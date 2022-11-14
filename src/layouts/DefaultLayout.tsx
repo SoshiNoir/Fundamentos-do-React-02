8013613c31a1473e00e24066f48e77fcfbc6a6b3
Soshi <migbonamim@gmail.com>
2022-11-14 05:17:11 -0300

    Layout de Rotas

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Outlet } from 'react-router-dom'
+import { Header } from '../Components/Header'
+
+export function DefaultLayout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  )
+}
