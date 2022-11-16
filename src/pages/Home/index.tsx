1ca470d26fad66086ec338efe3a9e2a065bf8014
Soshi <migbonamim@gmail.com>
2022-11-16 17:32:45 -0300

    Funcionalidades da aplicação-Iniciando novo ciclo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Play } from "phosphor-react";
+import { useState } from 'react';
 import { useForm } from "react-hook-form";
 import * as zod from 'zod';
 
@@ -23,8 +24,17 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 
 export function Home() {
+  const [ cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
   const {register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -34,9 +44,22 @@ export function Home() {
   })
 
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
-    reset();
+
+
+    const id = String(new Date().getTime())
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...cycles, newCycle])
+    setActiveCycleId(id)
+
+    reset()
   }
+
+  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
   const task = watch('task')
 
