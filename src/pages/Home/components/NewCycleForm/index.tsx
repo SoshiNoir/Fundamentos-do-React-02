d8ba5d5ec496a1a67be42006bd9827cc224e3a30
Soshi <migbonamim@gmail.com>
2022-11-17 19:29:53 -0300

    Convertendo para contexto

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,6 +1,27 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as zod from 'zod';
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles';
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
 export function NewCycleForm() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  });
+
   return (
     <FormContainer>
       <label htmlFor='task'>Vou trabalhar em</label>
@@ -8,7 +29,6 @@ export function NewCycleForm() {
         id='task'
         list='task-suggestions'
         placeholder='Dê um nome para o seu projeto'
-        disabled={!!activeCycle}
         {...register('task')}
       />
 
@@ -26,7 +46,6 @@ export function NewCycleForm() {
         step={5}
         min={5}
         max={60}
-        disabled={!!activeCycle}
         {...register('minutesAmount', { valueAsNumber: true })}
       />
 
