963ebb36997da5e70793d4a77bbda8d2f5efc569
Soshi <migbonamim@gmail.com>
2022-11-18 01:17:14 -0300

    Contexto entre rotas

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,68 +1,86 @@
-import { HistoryContainer, HistoryList, Status } from "./styles";
+import { useContext } from 'react';
+import { CyclesContext } from '../../contexts/CyclesContext';
+import { HistoryContainer, HistoryList, Status } from './styles';
 
 export function History() {
+  const { cycles } = useContext(CyclesContext);
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
 
-    <HistoryList>
-      <table>
-        <thead>
-          <tr>
-            <th>Tarefa</th>
-            <th>Duração</th>
-            <th>Início</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Tarefa</td>
-            <td>20 minutos</td>
-            <td>Há meses</td>
-            <td><Status statusColor="green">Concluído</Status></td>
-          </tr>
-          <tr>
-            <td>Tarefa</td>
-            <td>20 minutos</td>
-            <td>Há meses</td>
-            <td><Status statusColor="yellow">Em andamento</Status></td>
-          </tr>
-          <tr>
-            <td>Tarefa</td>
-            <td>20 minutos</td>
-            <td>Há meses</td>
-            <td><Status statusColor="red">Interrompido</Status></td>
-          </tr>
-          <tr>
-            <td>Tarefa</td>
-            <td>20 minutos</td>
-            <td>Há meses</td>
-            <td><Status statusColor="green">Concluído</Status></td>
-          </tr>
-          <tr>
-            <td>Tarefa</td>
-            <td>20 minutos</td>
-            <td>Há meses</td>
-            <td><Status statusColor="green">Concluído</Status></td>
-          </tr>
-          <tr>
-            <td>Tarefa</td>
-            <td>20 minutos</td>
-            <td>Há meses</td>
-            <td><Status statusColor="green">Concluído</Status></td>
-          </tr>
-          <tr>
-            <td>Tarefa</td>
-            <td>20 minutos</td>
-            <td>Há meses</td>
-            <td><Status statusColor="green">Concluído</Status></td>
-          </tr>
+      <pre>{JSON.stringify(cycles, null, 2)}</pre>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há meses</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há meses</td>
+              <td>
+                <Status statusColor='yellow'>Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há meses</td>
+              <td>
+                <Status statusColor='red'>Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há meses</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há meses</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há meses</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há meses</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
           </tbody>
-      </table>
-
-    </HistoryList>
+        </table>
+      </HistoryList>
     </HistoryContainer>
-
-  )
+  );
 }
